a2ddb00cbb4eebef265991b910b278c842b47ccf
100128112 <edward05.jang@gmail.com>
2019-10-01 15:55:36 -0500

    playing with pycharm

--- Learning.py
+++ Learning.py
@@ -1,2 +1,8 @@
-print("hello world.")
-print("howdy y'all!")
+# "Hello"
+# "This how you write notes using python"
+# "This is also called an Octothorpe
+# "When writing notes make sure that you put parenthesis and quotations around what you would like to write"
+
+# "Now you will be solving a math problem"
+# "When solving a math problem you must put parenthesis and enter in your math problem"
+# "I will show you an exapmle"
