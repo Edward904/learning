024d7c82aca7b67d7c36e0b98184570ba98b9077
100128112 <edward05.jang@gmail.com>
2019-10-02 15:15:35 -0500

    playing with pycharm

--- Learning.py
+++ Learning.py
@@ -1,4 +1,4 @@
-# "Hello"
+print("Hello World")
 # This how you write notes using python
 # This is also called an Octothorpe
 # When writing notes make sure that you put parenthesis and quotations around what you would like to write
@@ -6,4 +6,9 @@
 # Now you will be solving a math problem
 # When solving a math problem you must put parenthesis and enter in your math problem
 
+print("Here is the answer")
 print(7 * 5)
+# Now we must add a floating decimal
+print("Here is the floating decimal")
+print(7 * 5 / 1)
+
